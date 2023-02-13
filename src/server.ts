e0146933c646c7f76b14743f234bb6b03c117d5c
Serkan <newkipsoftware@gmail.com>
2023-02-13 05:13:38 +0300

    Fixes for NIP-11

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ class Server {
                     description: config.relay.description,
                     pubkey: config.relay.publicKey,
                     contact: config.relay.contact,
-                    supported_nips: 'NIP-01,NIP-14,NIP-11',
+                    supported_nips: [1, 11, 14],
                     software: 'Nostr Deno Server by Serkan KOCAMAN',
                     version: '0.1',
 
